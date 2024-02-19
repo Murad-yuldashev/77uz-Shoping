1800b813292686dad7ceb6380758a7516736ba5a
Murod-de <murad101693@gmail.com>
2024-02-19 09:48:57 +0500

    fix/added-carousel-js

---
+++ src/data/carousel-data.js
@@ -0,0 +1,9 @@
+export const carouselData = [
+    {id:1, img: '../../public/images/Img/cosmetics1.svg'},
+    {id:2, img: '../../public/images/Img/cosmetics2.svg'},
+    {id:3, img: '../../public/images/Img/cosmetics3.svg'},
+    {id:4, img: '../../public/images/Img/cosmetics4.svg'},
+    {id:5, img: '../../public/images/Img/cosmetics5.svg'},
+    {id:6, img: '../../public/images/Img/cosmetics6.svg'},
+    {id:7, img: '../../public/images/Img/cosmetics7.svg'},
+]
--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,4 +1,6 @@
-
+import { carouselData } from "../data/carousel-data";
+const carouselBoxHead = document.querySelector(".carousel-box-head");
+const carouselBoxBody = document.querySelector(".carousel-box-body");
 
 let swiper = new Swiper(".mySwiper", {
   loop: true,
@@ -20,4 +22,19 @@ let swiper2 = new Swiper(".mySwiper2", {
   },
 });
 
-console.log('hello');
+carouselData.forEach((carousel) => {
+  let carouselHead = `<div class="swiper-slide lg:!h-[481px] md:!h-[320px] !h-[266px]">
+                      <img src=${carousel.img} />
+                    </div>`;
+                  
+  carouselBoxHead.insertAdjacentHTML('beforeend', carouselHead);
+});
+
+carouselData.forEach((carousel) => {
+  let carouselBody = `<div class="swiper-slide">
+                        <img src=${carousel.img} />
+                      </div>`;
+
+  carouselBoxBody.insertAdjacentHTML(carouselBody);
+});
+console.log(carouselData);
